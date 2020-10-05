f179f8fd8cc0e487991809b214d1241ca1ef8ebd
Yura <yurii_kulachenko@mail.ru>
2020-10-05 23:15:52 +0300

    Добавляет моки

---
+++ src/mocks/offers.js
@@ -0,0 +1,161 @@
+export default [
+  {
+    id: 1704,
+    images: [`img/apartment-01.jpg`,
+      `img/apartment-01.jpg`,
+      `img/apartment-01.jpg`,
+      `img/apartment-01.jpg`,
+      `img/apartment-01.jpg`,
+      `img/apartment-01.jpg`],
+    accommodation: {
+      isPremium: true,
+      starsCount: `4.8`,
+      title: `Beautiful & luxurious apartment at great location`,
+      type: `appartment`,
+      bedroomsCount: `3`,
+      guestsLimit: `4`,
+      price: `120`,
+      features: [`Wi-Fi`,
+        `Heating`,
+        `Kitchen`,
+        `Fridge`,
+        `Washing mashine`,
+        `Coffee machine`,
+        `Dishwasher`,
+        `Towels`,
+        `Baby seat`,
+        `Cabel TV`
+      ]
+    },
+    host: {
+      avatar: `img/avatar-angelina.jpg`,
+      name: `Angellina`
+    },
+    description: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century. An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.`,
+    comments: [{
+      avatar: `img/avatar-max.jpg`,
+      name: `Max`,
+      starsCount: `4`,
+      commentText: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`,
+      date: new Date(`April, 2019`),
+    }]
+  },
+  {
+    id: 1705,
+    images: [`img/room.jpg`,
+      `img/room.jpg`,
+      `img/room.jpg`,
+      `img/room.jpg`,
+      `img/room.jpg`,
+      `img/room.jpg`],
+    accommodation: {
+      isPremium: false,
+      starsCount: `4.3`,
+      title: `Wood and stone place`,
+      type: `room`,
+      bedroomsCount: `2`,
+      guestsLimit: `2`,
+      price: `80`,
+      features: [`Wi-Fi`,
+        `Heating`,
+        `Kitchen`,
+        `Fridge`,
+        `Washing mashine`
+      ]
+    },
+    host: {
+      avatar: `img/avatar-angelina.jpg`,
+      name: `Angellina-2`
+    },
+    description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit, molestiae earum? Odio illo nemo maiores dolore officia beatae? Facere tempora aliquam quos ipsa, aspernatur cum exercitationem reprehenderit cupiditate ipsum molestiae.`,
+    comments: [{
+      avatar: `img/avatar-max.jpg`,
+      name: `Max-2`,
+      starsCount: `4`,
+      commentText: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit, molestiae earum? Odio illo nemo maiores dolore officia beatae? Facere tempora aliquam quos ipsa, aspernatur cum exercitationem reprehenderit cupiditate ipsum molestiae.`,
+      date: new Date(`April, 2019`),
+    }]
+  },
+  {
+    id: 1706,
+    images: [`img/apartment-02.jpg`,
+      `img/apartment-02.jpg`,
+      `img/apartment-02.jpg`,
+      `img/apartment-02.jpg`,
+      `img/apartment-02.jpg`,
+      `img/apartment-02.jpg`],
+    accommodation: {
+      isPremium: true,
+      starsCount: `4`,
+      title: `Canal View Prinsengracht`,
+      type: `house`,
+      bedroomsCount: `5`,
+      guestsLimit: `5`,
+      price: `180`,
+      features: [`Wi-Fi`,
+        `Heating`,
+        `Kitchen`,
+        `Fridge`,
+        `Washing mashine`,
+        `Coffee machine`,
+        `Dishwasher`,
+        `Towels`,
+        `Baby seat`,
+        `Cabel TV`
+      ]
+    },
+    host: {
+      avatar: `img/avatar-angelina.jpg`,
+      name: `Angellina-3`
+    },
+    description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit, molestiae earum? Odio illo nemo maiores dolore officia beatae? Facere tempora aliquam quos ipsa, aspernatur cum exercitationem reprehenderit cupiditate ipsum molestiae.`,
+    comments: [{
+      avatar: `img/avatar-max.jpg`,
+      name: `Max-3`,
+      starsCount: `4`,
+      commentText: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit, molestiae earum? Odio illo nemo maiores dolore officia beatae? Facere tempora aliquam quos ipsa, aspernatur cum exercitationem reprehenderit cupiditate ipsum molestiae.`,
+      date: new Date(`April, 2019`),
+    }]
+  },
+  {
+    id: 1707,
+    images: [`img/apartment-03.jpg`,
+      `img/apartment-03.jpg`,
+      `img/apartment-03.jpg`,
+      `img/apartment-03.jpg`,
+      `img/apartment-03.jpg`,
+      `img/apartment-03.jpg`],
+    accommodation: {
+      isPremium: true,
+      starsCount: `5`,
+      title: `Nice, cozy, warm big bed apartment`,
+      type: `hotel`,
+      bedroomsCount: `10`,
+      guestsLimit: `10`,
+      price: `200`,
+      features: [`Wi-Fi`,
+        `Heating`,
+        `Kitchen`,
+        `Fridge`,
+        `Washing mashine`,
+        `Coffee machine`,
+        `Dishwasher`,
+        `Towels`,
+        `Baby seat`,
+        `Cabel TV`
+      ]
+    },
+    host: {
+      avatar: `img/avatar-angelina.jpg`,
+      name: `Angellina-4`
+    },
+    description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit, molestiae earum? Odio illo nemo maiores dolore officia beatae? Facere tempora aliquam quos ipsa, aspernatur cum exercitationem reprehenderit cupiditate ipsum molestiae.`,
+    comments: [{
+      avatar: `img/avatar-max.jpg`,
+      name: `Max-4`,
+      starsCount: `5`,
+      commentText: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit, molestiae earum? Odio illo nemo maiores dolore officia beatae? Facere tempora aliquam quos ipsa, aspernatur cum exercitationem reprehenderit cupiditate ipsum molestiae.`,
+      date: new Date(`April, 2019`),
+    }]
+  },
+];
