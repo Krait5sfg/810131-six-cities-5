e05ede90046361533cda2d7609e361cb27b2ea10
Yura <yurii_kulachenko@mail.ru>
2020-10-05 23:25:36 +0300

    Подключает моки в index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
+import offers from './mocks/offers';
+import reviews from './mocks/reviews';
 
 const LEASE_COUNT = 312;
 
 ReactDOM.render(<App leaseCount={LEASE_COUNT} />, document.querySelector(`#root`));
-
