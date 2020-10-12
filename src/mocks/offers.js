a706b5f9585324c4001c61f0dd9c95be7cf5ab54
Yura <yurii_kulachenko@mail.ru>
2020-10-12 21:51:48 +0300

    Добавляет в моки предложений координаты

--- src/mocks/offers.js
+++ src/mocks/offers.js
@@ -36,6 +36,7 @@ export default [
     },
     description: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century. An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.`,
     isFavorite: true,
+    coordinates: [52.3909553943508, 4.85309666406198],
   },
   {
     city: `Amsterdam`,
@@ -67,6 +68,7 @@ export default [
     },
     description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit, molestiae earum? Odio illo nemo maiores dolore officia beatae? Facere tempora aliquam quos ipsa, aspernatur cum exercitationem reprehenderit cupiditate ipsum molestiae.`,
     isFavorite: true,
+    coordinates: [52.369553943508, 4.85309666406198],
   },
   {
     city: `Amsterdam`,
@@ -103,9 +105,10 @@ export default [
     },
     description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit, molestiae earum? Odio illo nemo maiores dolore officia beatae? Facere tempora aliquam quos ipsa, aspernatur cum exercitationem reprehenderit cupiditate ipsum molestiae.`,
     isFavorite: false,
+    coordinates: [52.3909553943508, 4.929309666406198],
   },
   {
-    city: `Brussels`,
+    city: `Amsterdam`,
     id: 1707,
     images: [`img/apartment-03.jpg`,
       `img/apartment-03.jpg`,
@@ -139,5 +142,6 @@ export default [
     },
     description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit, molestiae earum? Odio illo nemo maiores dolore officia beatae? Facere tempora aliquam quos ipsa, aspernatur cum exercitationem reprehenderit cupiditate ipsum molestiae.`,
     isFavorite: true,
+    coordinates: [52.3809553943508, 4.939309666406198],
   },
 ];
