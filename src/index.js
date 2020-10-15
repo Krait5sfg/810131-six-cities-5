339e59e60382e6da702be78575545f4f649bb412
Yura <yurii_kulachenko@mail.ru>
2020-10-15 22:54:55 +0300

    ActionCreator

--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './components/app/app';
 import offers from './mocks/offers';
 import reviews from './mocks/reviews';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {reducer} from './store/reducer';
 
-ReactDOM.render(<App offers={offers} reviews={reviews} />, document.querySelector(`#root`));
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f);
+
+ReactDOM.render(<Provider store={store}><App offers={offers} reviews={reviews} /></Provider>, document.querySelector(`#root`));
