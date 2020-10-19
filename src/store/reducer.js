d14c0cd436ca459040da1023391c4e42230a7f17
Yura <yurii_kulachenko@mail.ru>
2020-10-19 21:09:58 +0300

    Рефакторинг сортировки в reducer.js

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -25,16 +25,16 @@ const reducer = (state = initialState, action) => {
       return Object.assign({}, state, {sortingType: action.payload});
 
     case ActionType.SORT_LOW_TO_HIGH:
-      return Object.assign({}, state, {offers: state.offers.slice(0).sort((firstOffer, secondOffer) => firstOffer.accommodation.price > secondOffer.accommodation.price ? 1 : -1)});
+      return Object.assign({}, state, {offers: state.offers.slice(0).sort((firstOffer, secondOffer) => firstOffer.accommodation.price - secondOffer.accommodation.price)});
 
     case ActionType.SORT_POPULAR:
       return Object.assign({}, state, {offers: offers.filter((offer) => offer.city === state.city)});
 
     case ActionType.SORT_HIGH_TO_LOW:
-      return Object.assign({}, state, {offers: state.offers.slice(0).sort((firstOffer, secondOffer) => firstOffer.accommodation.price > secondOffer.accommodation.price ? -1 : 1)});
+      return Object.assign({}, state, {offers: state.offers.slice(0).sort((firstOffer, secondOffer) => secondOffer.accommodation.price - firstOffer.accommodation.price)});
 
     case ActionType.SORT_TOP_RATED:
-      return Object.assign({}, state, {offers: state.offers.slice(0).sort((firstOffer, secondOffer) => firstOffer.accommodation.rating > secondOffer.accommodation.rating ? -1 : 1)});
+      return Object.assign({}, state, {offers: state.offers.slice(0).sort((firstOffer, secondOffer) => secondOffer.accommodation.rating - firstOffer.accommodation.rating)});
 
     case ActionType.UPDATE_ID_ACTIVE_CARD_FOR_MAP:
       return Object.assign({}, state, {idActiveCardForMap: action.payload});
