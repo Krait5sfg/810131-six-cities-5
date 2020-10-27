7920d60aababd66faa076ee183fcfef37218f884
Yura <yurii_kulachenko@mail.ru>
2020-10-27 20:27:25 +0300

    Новое действие в api-action

--- src/store/api-actions.js
+++ src/store/api-actions.js
@@ -23,3 +23,7 @@ export const checkAuth = () => (dispatch, _getState, api) => (
     })
 );
 
+export const login = ({login: email, password}) => (dispatch, _getState, api) => (
+  api.post(Request.AUTHORIZATION, {email, password})
+    .then(() => dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH)))
+);
