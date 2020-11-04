69e4a1ed5b75955a63028d285cf8b5947567a11f
Yura <yurii_kulachenko@mail.ru>
2020-11-04 13:19:44 +0300

    e2e test for User

---
+++ src/components/user/user.e2e.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {User} from './user';
+
+configure({adapter: new Adapter()});
+
+it(`Click by user's email`, () => {
+  const onLinkEmailClick = jest.fn();
+
+  const wrapper = shallow(<User
+    onLinkEmailClick={onLinkEmailClick}
+    authorizationStatus={true}
+    userData={{fake: true}}
+  />
+  );
+
+  wrapper.find(`.header__nav-link`).simulate(`click`);
+  expect(onLinkEmailClick).toHaveBeenCalledTimes(1);
+});
